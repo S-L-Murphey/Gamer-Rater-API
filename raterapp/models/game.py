0e1d0fe8bda6df84bfd108467f84e8f079285af2
S-L-Murphey <smurphey1990@gmail.com>
2021-08-06 13:17:00 -0500

    Added fixtures for all models

--- raterapp/models/game.py
+++ raterapp/models/game.py
@@ -7,9 +7,10 @@ class Game(models.Model):
     """
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=150)
-    creator = models.ForeignKey("User", on_delete=models.CASCADE)
+    user_id = models.ForeignKey("User", on_delete=models.CASCADE)
     year_released = DateField()
     number_of_players = models.IntegerField()
     duration = models.IntegerField()
     categories = models.ManyToManyField("Category", through="JoinCategory", related_name="category")
     age_rec = models.IntegerField()
+    creator = models.CharField(max_length=100)
